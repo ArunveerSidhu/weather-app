2319bfb12cdc0a43ce1d807e9b88126e0c2f585b
ArunveerSidhu <sidhu.arunveer@gmail.com>
2025-01-16 00:34:33 +0530

    done with the search bar ui

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,41 +3,28 @@ import React from "react";
 function Search() {
   return (
     <>
-      <div className=" h-16 w-full bg-cyan-900 flex justify-center align-center">
-        <form class="w-7xl">
-          <label
-            for="default-search"
-            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      <div className="h-16 bg-indigo-700 flex items-center justify-center">
+        <div className="relative flex items-center">
+          <svg 
+            className="absolute left-3 w-5 h-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor"
           >
-            Search
-          </label>
-          <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="search"
-              id="default-search"
-              class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Mockups, Logos..."
-              required
+            <path 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              strokeWidth={2} 
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" 
             />
-          </div>
-        </form>
+          </svg>
+        <input 
+        type="text" 
+        placeholder="Search" 
+        className="w-96 h-10 rounded-md bg-white outline-none px-10 font-serif"
+          />
+        </div>
       </div>
     </>
   );
